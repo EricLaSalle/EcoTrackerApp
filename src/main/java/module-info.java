3e923691e274610ba990156592cb8753b52f9c0e
Eric <eric.lorenzo@gracia.lasalle.cat>
2025-05-20 11:01:21 +0200

    commit inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.example.ecotrackerapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.ecotrackerapp to javafx.fxml;
+    exports org.example.ecotrackerapp;
+    exports org.example.ecotrackerapp.controllers;
+    opens org.example.ecotrackerapp.controllers to javafx.fxml;
+}
