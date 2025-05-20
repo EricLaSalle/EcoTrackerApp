41b791f4daafd4debaca3f8b72dd46d60d43a9bb
Eric <eric.lorenzo@gracia.lasalle.cat>
2025-05-20 20:46:24 +0200

    Creem nova escena a partir de clicar el boto

--- src/main/java/org/example/ecotrackerapp/Main.java
+++ src/main/java/org/example/ecotrackerapp/Main.java
@@ -5,9 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class Main extends Application {
 
@@ -18,32 +15,14 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/inici.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Inici");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-    /**
-     * Aquest mètode estableix la connexió amb la base de dades.
-     */
-    public static void conectarAmbBBDD() {
-        // Declaració de variables
-        String url = "jdbc:mysql://localhost:3306/ecotrackerapp";
-        String usuari = "root";
-        String password = "";
-
-        try {
-            //Establir la connexió
-            Connection conn = DriverManager.getConnection(url, usuari, password);
-            System.out.println("Connexió amb èxit a la base de dades.");
-        }catch (SQLException sqle){
-            System.out.println("Error al connectar a la base de dades: " + sqle.getMessage());
-        }
     }
 }
--- src/main/java/org/example/ecotrackerapp/controllers/Controller.java
+++ src/main/java/org/example/ecotrackerapp/controllers/Controller.java
@@ -1,12 +1,53 @@
 package org.example.ecotrackerapp.controllers;
 
 import javafx.fxml.FXML;
-import static org.example.ecotrackerapp.Main.conectarAmbBBDD;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.example.ecotrackerapp.Main;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class Controller {
 
+    /**
+     * Aquest mètode actúa a l'inici de tot quan es prem el botó "Obrir App".
+     */
     @FXML
-    protected void onOpenAppButtonClick() {
+    protected void onOpenAppButtonClick() throws IOException {
         conectarAmbBBDD();
+        pantallaMenu();
+    }
+
+    /**
+     * Aquest mètode estableix la connexió amb la base de dades.
+     */
+    protected void conectarAmbBBDD() {
+        // Declaració de variables
+        String url = "jdbc:mysql://localhost:3306/ecotrackerapp";
+        String usuari = "root";
+        String password = "";
+
+        try {
+            //Establir la connexió
+            Connection conn = DriverManager.getConnection(url, usuari, password);
+            System.out.println("Connexió amb èxit a la base de dades.");
+        }catch (SQLException sqle){
+            System.out.println("Error al connectar a la base de dades: " + sqle.getMessage());
+        }
+    }
+
+    protected void pantallaMenu() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Controller.class.getResource("/org/example/ecotrackerapp/view/menu.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Ecotracker App");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
     }
 }
