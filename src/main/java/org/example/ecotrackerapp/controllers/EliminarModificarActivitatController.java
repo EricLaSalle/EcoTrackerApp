5ac951b277f7c980862a8cc8cd3790b665ae7591
Eric <eric.lorenzo@gracia.lasalle.cat>
2025-05-22 10:34:12 +0200

    afegim els valors de la taula al controlador

--- src/main/java/org/example/ecotrackerapp/controllers/EliminarModificarActivitatController.java
+++ src/main/java/org/example/ecotrackerapp/controllers/EliminarModificarActivitatController.java
@@ -1,4 +1,39 @@
 package org.example.ecotrackerapp.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.example.ecotrackerapp.model.ActivitatsSostenibles;
+
 public class EliminarModificarActivitatController {
+    //Declaració de valors fxml
+    @FXML private TableView<ActivitatsSostenibles> tablaActividades;
+    @FXML private TableColumn<ActivitatsSostenibles, Integer> colId;
+    @FXML private TableColumn<ActivitatsSostenibles, String> colNom;
+    @FXML private TableColumn<ActivitatsSostenibles, String> colData;
+    @FXML private TableColumn<ActivitatsSostenibles, String> colCategoria;
+    @FXML private TableColumn<ActivitatsSostenibles, String> colDescripcio;
+    @FXML private TableColumn<ActivitatsSostenibles, Double> colQuantitat;
+    @FXML private TableColumn<ActivitatsSostenibles, Double> colCo2;
+
+    /**
+     * Aquest mètode inicialitza la vista de la llista d'activitats sostenibles.
+     */
+    public void initialize() {
+        // Configurar columnes
+        colId.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleIntegerProperty(cellData.getValue().getId()).asObject());
+        colNom.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getNom()));
+        colData.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getData().toString()));
+        colCategoria.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getCategoria().getNomCategoria()));
+        colDescripcio.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getDescripcio()));
+        colQuantitat.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleDoubleProperty(cellData.getValue().getQuantitat()).asObject());
+        colCo2.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleDoubleProperty(cellData.getValue().getCo2TotalEstalviat()).asObject());
+    }
 }
